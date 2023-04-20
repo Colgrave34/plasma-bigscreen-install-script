38e497ec6d984111d07963262349b08e0100cc17
Colgrave <hanqixu.blogs@simplelogin.co>
2023-04-20 16:07:57 -0400

    update stella.sh

--- stella.sh
+++ stella.sh
@@ -1,1 +1,43 @@
 #!/bin/bash
+echo "Installing SDDM as display manager..."
+sudo pacman -Syu sddm
+
+echo "Installing dependencies..."
+sudo pacman -Syu base-devel plasma-pa plasma-nm konsole systemsettings
+curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/extra-cmake-modules-git.tar.gz
+tar -xvf extra-cmake-modules-git.tar.gz
+cd extra-cmake-modules-git
+makepkg -sir
+cd
+
+echo "Building and installing KDE Bigscreen..."
+curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/plasma-bigscreen-git.tar.gz
+tar -xvf plasma-bigscreen-git.tar.gz
+cd plasma-bigscreen-git
+makepkg -sir
+cd
+
+echo "Cleaning up..."
+rm -r extra-cmake-modules-git/ &&
+rm -r plasma-bigscreen-git/ &&
+rm extra-cmake-modules-git.tar.gz  plasma-bigscreen-git.tar.gz
+sudo pacman -Rs extra-cmake-modules-git
+
+echo "Installing Kodi"
+sudo pacman -S kodi
+
+echo "Enabling multilib and install steam..."
+sudo pacman -Syu steam
+
+echo "Installing Flatpak"
+sudo pacman -S flatpak
+
+echo Install completed! 
+echo Please run SDDM and make sure everything running correctly. 
+echo To run SDDM:
+echo sudo systemctl start sddm
+echo To enable SDDM:
+echo sudo systemctl enable sddm
+
+echo If you'd like to autologin, consider to read this part of the wiki. 
+echo https://wiki.archlinux.org/title/SDDM#Autologin
