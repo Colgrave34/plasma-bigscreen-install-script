b16534b5559c0162756056d465d409f1ed673526
Colgrave34 <hanqixu.blogs@simplelogin.co>
2023-06-26 18:01:35 -0400

    Fix while loop not ending problem, and fix format

--- stella.sh
+++ stella.sh
@@ -18,7 +18,6 @@ echo -e "This post-install script only provides the essentials for Plasma Bigscr
 echo "Checking updates..."
 sleep 2
 echo "y" | sudo pacman -Syu
-echo
 
 
 # Ask user input for installing SDDM
@@ -26,12 +25,11 @@ while true
 do
     read -p "Do you want to install SDDM display manager? (y/n): " sddm
     case $sddm in
-        Y|y ) echo "Installing SDDM..."; sleep 2; echo -e "\ny" | sudo pacman -Syu sddm;;
+        Y|y ) echo "Installing SDDM..."; sleep 2; echo -e "\ny" | sudo pacman -Syu sddm; break;;
         N|n ) break;;
         * ) echo "Invalid input";;
     esac
 done
-echo
 
 
 # Ask user input for installing Kodi
@@ -39,12 +37,11 @@ while true
 do
     read -p "Do you want to install Kodi? (y/n): " kodi
     case $kodi in
-        Y|y ) echo "Installing Kodi..."; sleep 2; yes | sudo pacman -Syu kodi;;
+        Y|y ) echo "Installing Kodi..."; sleep 2; yes | sudo pacman -Syu kodi; break;;
         N|n ) break;;
         * ) echo "Invalid input";;
     esac
 done
-echo
 
 
 # Ask user input for installing Steam
@@ -52,19 +49,17 @@ while true
 do
     read -p "Do you want to install Steam? (y/n): " steam
     case $steam in
-        Y|y ) echo "Enabling multilib and install Steam..."; sleep 2; echo -e "[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf; echo -e "\n\n\ny" | sudo pacman -Syu steam;;
+        Y|y ) echo "Enabling multilib and install Steam..."; sleep 2; echo -e "[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf; echo -e "\n\n\ny" | sudo pacman -Syu steam; break;;
         N|n ) break;;
         * ) echo "Invalid input";;
     esac
 done
-echo
 
 
 # Install dependencies
 echo "Installing dependencies..."
 sleep 2
 echo -e "\n\n\n\ny" | sudo pacman -Syu base-devel fakeroot plasma-pa plasma-nm konsole systemsettings
-echo
 
 
 # Install extra-cmake-modules dependency
@@ -73,7 +68,6 @@ tar -xvf extra-cmake-modules-git.tar.gz
 cd extra-cmake-modules-git
 yes | makepkg -sir
 cd
-echo
 
 
 # Build KDE Bigscreen
@@ -84,7 +78,6 @@ tar -xvf plasma-bigscreen-git.tar.gz
 cd plasma-bigscreen-git
 yes | makepkg -sir
 cd
-echo
 
 
 # Clean up
@@ -93,11 +86,9 @@ sleep 2
 yes | rm -r extra-cmake-modules-git/ plasma-bigscreen-git/
 yes | rm extra-cmake-modules-git.tar.gz  plasma-bigscreen-git.tar.gz
 yes | sudo pacman -Rs extra-cmake-modules-git && \
-echo
 
 
 echo "Install completed!"
 echo "Please run SDDM and make sure everything running correctly."
 echo "If you'd like to autologin, consider to read this part of the wiki."
 echo "https://wiki.archlinux.org/title/SDDM#Autologin"
-echo
