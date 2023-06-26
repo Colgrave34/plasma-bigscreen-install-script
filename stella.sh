0c4856fab27dbec91d84b3128770f38033c8d81b
Colgrave34 <hanqixu.blogs@simplelogin.co>
2023-06-26 15:38:48 -0400

    Update stella.sh

--- stella.sh
+++ stella.sh
@@ -19,55 +19,63 @@ while true
 do
     read -p "Do you want to install SDDM display manager? (y/n): " sddm
     case $sddm in
-        Y|y ) sddm=true; break;;
+        Y|y ) echo "Installing SDDM"; sleep 2; echo -e  "\ny" | sudo pacman -Syu sddm;;
         N|n ) break;;
         * ) echo "Invalid input";;
     esac
 done
 
 
-# Install dependencies. 
+# Install dependencies.
 echo -e "Installing dependencies...\n"
 sleep 2
-sudo pacman -Syu base-devel fakeroot plasma-pa plasma-nm konsole systemsettings
+echo -e "\n\n\n\ny" | sudo pacman -Syu base-devel fakeroot plasma-pa plasma-nm konsole systemsettings
+
+# Install extra-cmake-modules dependency.
 curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/extra-cmake-modules-git.tar.gz
 tar -xvf extra-cmake-modules-git.tar.gz
 cd extra-cmake-modules-git
 makepkg -sir
 cd
 
-# echo "Building and installing KDE Bigscreen..."
-# curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/plasma-bigscreen-git.tar.gz
-# tar -xvf plasma-bigscreen-git.tar.gz
-# cd plasma-bigscreen-git
-# makepkg -sir
-# cd
 
-# echo "Cleaning up..."
-# rm -r extra-cmake-modules-git/ &&
-# rm -r plasma-bigscreen-git/ &&
-# rm extra-cmake-modules-git.tar.gz  plasma-bigscreen-git.tar.gz
-# sudo pacman -Rs extra-cmake-modules-git
+# Build KDE Bigscreen.
+echo "Building and installing KDE Bigscreen..."
+sleep 2
+curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/plasma-bigscreen-git.tar.gz
+tar -xvf plasma-bigscreen-git.tar.gz
+cd plasma-bigscreen-git
+makepkg -sir
+cd
 
-# echo "Installing Kodi"
-# sudo pacman -S kodi
 
-# echo "Enabling multilib and install steam..."
-# sudo bash -c 'cat << EOF >> /etc/pacman.conf
-# [multilib]
-# Include = /etc/pacman.d/mirrorlist
-# EOF'
-# sudo pacman -Syu steam
+# Clean up.
+echo "Cleaning up..."
+sleep 2
+rm -r extra-cmake-modules-git/ &&
+rm -r plasma-bigscreen-git/ &&
+rm extra-cmake-modules-git.tar.gz  plasma-bigscreen-git.tar.gz
+sudo pacman -Rs extra-cmake-modules-git
 
-# echo "Installing Flatpak"
-# sudo pacman -S flatpak
+echo "Installing Kodi"
+sudo pacman -S kodi
 
-# echo "Install completed!"
-# echo "Please run SDDM and make sure everything running correctly."
-# echo "To run SDDM:"
-# echo "sudo systemctl start sddm"
-# echo "To enable SDDM:"
-# echo "sudo systemctl enable sddm"
+echo "Enabling multilib and install steam..."
+sudo bash -c 'cat << EOF >> /etc/pacman.conf
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+EOF'
+sudo pacman -Syu steam
 
-# echo "If you'd like to autologin, consider to read this part of the wiki."
-# echo "https://wiki.archlinux.org/title/SDDM#Autologin"
+echo "Installing Flatpak"
+sudo pacman -S flatpak
+
+echo "Install completed!"
+echo "Please run SDDM and make sure everything running correctly."
+echo "To run SDDM:"
+echo "sudo systemctl start sddm"
+echo "To enable SDDM:"
+echo "sudo systemctl enable sddm"
+
+echo "If you'd like to autologin, consider to read this part of the wiki."
+echo "https://wiki.archlinux.org/title/SDDM#Autologin"
